7c1aa81d77360db372c289cfd65c8fa37c032a9a
lpearf-pixel <lpearf@gmail.com>
2026-08-14 10:04:02 +0800

    feat: export care query and revision contracts

--- packages/contracts/src/care/index.ts
+++ packages/contracts/src/care/index.ts
@@ -5,3 +5,5 @@ export * from './diaper.js';
 export * from './sleep.js';
 export * from './actions.js';
 export * from './measurements.js';
+export * from './query.js';
+export * from './revisions.js';
